0b7757cefcf4275edafb76d07f29ebbc1ac95555
Raj Pathar <raj.pathar93@gmail.com>
2026-03-23 08:26:54 +0530

    c

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { ContactModule } from './contact/contact.module';
-import { AppointmentModule } from './appointment/appointment.module';
 import { GoogleController } from './google/google.controller';
 import { GoogleService } from './google/google.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    ContactModule,
-    AppointmentModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController, GoogleController],
   providers: [AppService, GoogleService],
 })
